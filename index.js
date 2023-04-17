2601ad2533033e366435a9b229a345afba767d6f
Divilisrikanth <srikanthdivili1994@gmail.com>
2023-04-17 17:23:58 +0530

    pushing this code into github repo

---
+++ index.js
@@ -0,0 +1,30 @@
+let count =1;
+
+let element =document.getElementsByTagName("body")[0];
+
+let heading =document.getElementById("heading");
+
+function green(){
+    element.style.background="green";
+    heading.innerHTML="Back ground color is green";
+}
+function red(){
+    element.style.background="red"
+    heading.innerHTML="Back ground color is red";
+}
+function yellow(){
+    element.style.background="Yellow"
+    heading.innerHTML="Background is yellow"
+}
+function pink(){
+    element.style.background="pink"
+    heading.innerHTML="Background color is pink"
+}
+function orange(){
+    element.style.background="orange"
+    heading.innerHTML="Background color is orange"
+}
+
+
+
+
